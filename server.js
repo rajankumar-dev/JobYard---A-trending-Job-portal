5626cc03870210a34ec9406cbbfffe2ff2f228d5
rajankumar-dev <rk1620703@gmail.com>
2025-12-16 21:55:29 +0530

    connection established success

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -10,12 +11,18 @@ const port = process.env.PORT || 8080;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+})
+.catch((error) => {
+    console.error("Failed to connect to database:", error);
+    process.exit(1);
+});
+
 // Sample route
 app.get('/', (req, res) => {
     res.send('Hello World!');
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
 });
