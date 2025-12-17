9e39b9fb70358c86b53aaadbb34690978960d82b
rajankumar-dev <rk1620703@gmail.com>
2025-12-17 14:12:38 +0530

    authorization

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+const userAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    next("Unauthorized: No token provided");
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (error) {
+    next("Unauthorized: Invalid token");
+  }
+};
+export default userAuth;
--- routes/test.route.js
+++ routes/test.route.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import {testController} from '../controllers/test.controller.js';
+import express from "express";
+import { testController } from "../controllers/test.controller.js";
+import userAuth from "../middlewares/authMiddleware.js";
 
 //route object
 const router = express.Router();
 
 //test route
-router.get('/test', testController);
+router.post("/test", testController);
 
 export default router;
