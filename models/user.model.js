05470374e6c873e3109d67fd819055842d60b970
rajankumar-dev <rk1620703@gmail.com>
2025-12-17 12:05:02 +0530

    hash password using bcryptjs

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -29,4 +30,9 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+//middleware to hash password before saving
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 export const User = mongoose.model("User", userSchema);
