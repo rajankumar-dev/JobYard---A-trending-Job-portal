918ea91ce0d640ae2d1ccc80038005cccad5d117
rajankumar-dev <rk1620703@gmail.com>
2025-12-18 10:26:58 +0530

    complete pagination  functionality

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -47,13 +47,24 @@ export const getAllJobsController = async (req, res, next) => {
   if (sort === "z-a") {
     queryResult = queryResult.sort("-position");
   }
+
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  queryResult = queryResult.skip(skip).limit(limit);
+  //job Count
+  const totalJobs = await Job.countDocuments(queryResult);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
   const jobs = await queryResult;
 
   //   const jobs = await Job.find({ createdBy: req.user.id });
   res.status(200).send({
-    totalJobs: jobs.length,
+    totalJobs,
     success: true,
     jobs,
+    numOfPages,
   });
 };
 
