7cb21213ca03c3cfcb8757342d9fa0389b6e2c83
rajankumar-dev <rk1620703@gmail.com>
2025-12-18 10:18:46 +0530

    complete searching and sorting  functionality

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -19,7 +19,37 @@ export const createJobController = async (req, res, next) => {
 
 // ========= get job controller ===========
 export const getAllJobsController = async (req, res, next) => {
-  const jobs = await Job.find({ createdBy: req.user.id });
+  const { status, workType, search, sort } = req.query;
+
+  //condition for status
+  let queryObject = { createdBy: req.user.id };
+  if (status && status !== "all") {
+    queryObject.status = status;
+  }
+  if (workType && workType !== "all") {
+    queryObject.workType = workType;
+  }
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" };
+  }
+  let queryResult = Job.find(queryObject);
+
+  //sort
+  if (sort === "latest") {
+    queryResult = queryResult.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    queryResult = queryResult.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    queryResult = queryResult.sort("position");
+  }
+  if (sort === "z-a") {
+    queryResult = queryResult.sort("-position");
+  }
+  const jobs = await queryResult;
+
+  //   const jobs = await Job.find({ createdBy: req.user.id });
   res.status(200).send({
     totalJobs: jobs.length,
     success: true,
