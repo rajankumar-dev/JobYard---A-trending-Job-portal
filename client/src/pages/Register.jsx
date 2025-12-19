ab139d7aebf675fde60e5a9476a8bdf378d07e48
rajankumar-dev <rk1620703@gmail.com>
2025-12-19 14:17:58 +0530

    feat(client): create reusable InputForm component and integrate it into the Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import InputFrom from "../components/shared/InputForm";
 
 const Register = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // handle input change
+  // const handleInputChange = (e) => {
+  //   setFormData({ ...formData, [e.target.name]: e.target.value });
+  // };
 
   //form  function
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      console.log(
-        formData.name,
-        formData.lastName,
-        formData.email,
-        formData.password
-      );
+      console.log(name, lastName, email, password);
     } catch (error) {
       console.log(error);
     }
@@ -27,63 +26,38 @@ const Register = () => {
     <div className="form-container card">
       <img src="/images/logo.png" alt="" className="reg-logo" />
       <form onSubmit={handleSubmit}>
-        <div class="mb-2">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          />
-        </div>
-        <div class="mb-2">
-          <label htmlFor="lastName" className="form-label">
-            Last Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="lastName"
-            name="lastName"
-            value={formData.lastName}
-            onChange={(e) =>
-              setFormData({ ...formData, lastName: e.target.value })
-            }
-          />
-        </div>
-        <div class="mb-2">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={(e) =>
-              setFormData({ ...formData, email: e.target.value })
-            }
-          />
-        </div>
-        <div class="mb-2">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={formData.password}
-            onChange={(e) =>
-              setFormData({ ...formData, password: e.target.value })
-            }
-          />
-        </div>
+        <InputFrom
+          htmlFor="name"
+          labelText={"Name"}
+          type={"text"}
+          value={name}
+          handleChange={(e) => setName(e.target.value)}
+          name="name"
+        />
+        <InputFrom
+          htmlFor="lastName"
+          labelText={"Last Name"}
+          type={"text"}
+          value={lastName}
+          handleChange={(e) => setLastName(e.target.value)}
+          name="lastName"
+        />
+        <InputFrom
+          htmlFor="email"
+          labelText={"Email"}
+          type={"email"}
+          value={email}
+          handleChange={(e) => setEmail(e.target.value)}
+          name="email"
+        />
+        <InputFrom
+          htmlFor="password"
+          labelText={"Password"}
+          type={"password"}
+          value={password}
+          handleChange={(e) => setPassword(e.target.value)}
+          name="password"
+        />
         {/* <div class="mb-2">
           <label className="form-label" htmlFor="location">
             Location
