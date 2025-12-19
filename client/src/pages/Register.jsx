f011a33274541b1a30b0c6da9d81667ae383b392
rajankumar-dev <rk1620703@gmail.com>
2025-12-19 22:28:40 +0530

    feat(client): add Spinner component and integrate loading state in Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import InputFrom from "../components/shared/InputForm";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { showLoading, hideLoading } from "../redux/features/alertSlice";
 import axios from "axios";
+import Spinner from "./../components/shared/Spinner";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -15,6 +16,9 @@ const Register = () => {
   // const handleInputChange = (e) => {
   //   setFormData({ ...formData, [e.target.name]: e.target.value });
   // };
+
+  //redux state
+  const { loading } = useSelector((state) => state.alerts);
 
   //hooks
   const dispatch = useDispatch();
@@ -53,51 +57,57 @@ const Register = () => {
     }
   };
   return (
-    <div className="form-container card">
-      <img src="/images/logo.png" alt="" className="reg-logo" />
-      <form onSubmit={handleSubmit}>
-        <InputFrom
-          htmlFor="name"
-          labelText={"Name"}
-          type={"text"}
-          value={name}
-          handleChange={(e) => setName(e.target.value)}
-          name="name"
-        />
-        <InputFrom
-          htmlFor="lastName"
-          labelText={"Last Name"}
-          type={"text"}
-          value={lastName}
-          handleChange={(e) => setLastName(e.target.value)}
-          name="lastName"
-        />
-        <InputFrom
-          htmlFor="email"
-          labelText={"Email"}
-          type={"email"}
-          value={email}
-          handleChange={(e) => setEmail(e.target.value)}
-          name="email"
-        />
-        <InputFrom
-          htmlFor="password"
-          labelText={"Password"}
-          type={"password"}
-          value={password}
-          handleChange={(e) => setPassword(e.target.value)}
-          name="password"
-        />
-        <div className="d-flex btn-container">
-          <button type="submit" className="btn btn-primary">
-            Register
-          </button>
-          <p>
-            Already a member? <Link to="/login">Login </Link>
-          </p>
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="form-container card">
+          <img src="/images/logo.png" alt="" className="reg-logo" />
+          <form onSubmit={handleSubmit}>
+            <InputFrom
+              htmlFor="name"
+              labelText={"Name"}
+              type={"text"}
+              value={name}
+              handleChange={(e) => setName(e.target.value)}
+              name="name"
+            />
+            <InputFrom
+              htmlFor="lastName"
+              labelText={"Last Name"}
+              type={"text"}
+              value={lastName}
+              handleChange={(e) => setLastName(e.target.value)}
+              name="lastName"
+            />
+            <InputFrom
+              htmlFor="email"
+              labelText={"Email"}
+              type={"email"}
+              value={email}
+              handleChange={(e) => setEmail(e.target.value)}
+              name="email"
+            />
+            <InputFrom
+              htmlFor="password"
+              labelText={"Password"}
+              type={"password"}
+              value={password}
+              handleChange={(e) => setPassword(e.target.value)}
+              name="password"
+            />
+            <div className="d-flex btn-container">
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+              <p>
+                Already a member? <Link to="/login">Login </Link>
+              </p>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      )}
+    </>
   );
 };
 
