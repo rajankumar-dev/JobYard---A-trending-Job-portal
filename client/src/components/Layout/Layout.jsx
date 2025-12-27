e05f3db275497d0a63cd89a798153c3f65fb3fc2
rajankumar-dev <rk1620703@gmail.com>
2025-12-27 08:37:45 +0530

    feat(layout): enhance Layout component with sidebar menu and styling

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import "../../styles/Layout.css";
+import { UserMenu } from "./Menus/UserMenu";
+import { useLocation, Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Layout = ({ children }) => {
+  const sidebarMenu = UserMenu;
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <>
       <div className="row">
-        <div className="col-md-3">Menu</div>
+        <div className="col-md-3 sidebar">
+          <div className="logo">
+            <h6>JOB PORTAL</h6>
+          </div>
+          <hr />
+          <p className="username">Welcome : username</p>
+          <hr />
+          <div className="menu">
+            {sidebarMenu.map((menu) => {
+              const isActive = location.pathname === menu.path;
+              return (
+                <div
+                  className={`menu-item ${isActive && "active"}`}
+                  key={menu.name}
+                >
+                  <i className={menu.icon}></i>
+                  <Link to={menu.path}>{menu.name}</Link>
+                </div>
+              );
+            })}
+            <div
+              className={`menu-item`}
+              onClick={() => {
+                localStorage.clear();
+                toast.success("Logout Successfully");
+                navigate("/login");
+              }}
+            >
+              <i className="fa-solid fa-right-from-bracket"></i>
+              <Link to="/login">Logout</Link>
+            </div>
+          </div>
+        </div>
         <div className="col-md-3">{children}</div>
       </div>
     </>
---
+++ client/src/components/Layout/Menus/UserMenu.jsx
@@ -0,0 +1,17 @@
+export const UserMenu = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: "fa-solid fa-house",
+  },
+  {
+    name: "latest Jobs",
+    path: "/jobs",
+    icon: "fa-solid fa-briefcase",
+  },
+  {
+    name: "update Profile",
+    path: "/user/profile",
+    icon: "fa-solid fa-user",
+  },
+];
